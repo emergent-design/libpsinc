a638370f486fa2fbfdf2302a10f8fbe9c82cda58
Dan Parnham <dan@emergent-design.co.uk>
2014-08-09 16:20:16 +0100

    Added the ability to override the monochrome flag in the image handler.

--- cpp/include/psinc/handlers/ImageHandler.h
+++ cpp/include/psinc/handlers/ImageHandler.h
@@ -11,9 +11,9 @@ namespace psinc
 	class ImageHandler : public DataHandler
 	{
 		public:
-			
-			ImageHandler() {}
-			
+
+			ImageHandler(bool forceBayer = false) : forceBayer(forceBayer) {}
+
 			/// Constructor
 			/// @param[in] image Reference to an image buffer that will be populated with the converted data.
 			ImageHandler(emg::ImageBase<byte> &image);
@@ -26,8 +26,9 @@ namespace psinc
 			virtual bool Process(bool monochrome, emg::Buffer<byte> &data, int width, int height);
 
 		protected:
-			
+
 			/// The image to be populated.
 			emg::ImageBase<byte> *image = nullptr;
+			bool forceBayer;
 	};
 }
--- cpp/src/psinc/handlers/ImageHandler.cpp
+++ cpp/src/psinc/handlers/ImageHandler.cpp
@@ -10,30 +10,33 @@ namespace psinc
 	{
 		this->image = &image;
 	}
-	
-	
+
+
 	void ImageHandler::Initialise(ImageBase<byte> &image)
 	{
 		this->image = &image;
 	}
-	
-	
+
+
 	bool ImageHandler::Process(bool monochrome, emg::Buffer<byte> &data, int width, int height)
 	{
+		// Allow the monochrome flag to be overridden - could have unexpected effects.
+		if (this->forceBayer) monochrome = false;
+
 		int w = monochrome ? width : width - 4;
 		int h = monochrome ? height : height - 4;
-		
+
 		if (w > 0 && h > 0 && data.Size() == width * height)
 		{
 			this->image->Resize(w, h);
-			
+
 			if (monochrome)						this->DecodeMono(data, this->image->Data(), width, height, this->image->Depth());
 			else if (this->image->Depth() == 3)	this->DecodeColour(data, this->image->Data(), width, height);
 			else								this->DecodeGrey(data, this->image->Data(), width, height);
-			
+
 			return true;
 		}
-		
+
 		return false;
 	}
 }
