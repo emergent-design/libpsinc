a92a64a80d6a56dd2580c2ec0bb5e467f0975cb2
Dan <dan@emergent-design.co.uk>
2014-09-10 00:23:50 +0100

    Added a premake5 based config file with alternative support for MSVC (debug/release and x32/x64) without affecting the default stripped library configuration used under linux.

--- cpp/premake5.lua
+++ cpp/premake5.lua
@@ -1,28 +1,37 @@
 solution "psinc"
 	language		"C++"
 	targetdir		"lib"
-	configurations	"default"
-	platforms		"native"
 	includedirs		"include"
-	libdirs			"lib"
+	libdirs			{ "lib" }
 	excludes		{ "**.bak", "**~" }
+	
+	configuration "not vs*"
+		configurations  { "default" }
+		platforms		"native"
+	configuration "vs*"
+		configurations  { "debug", "release" }
+		platforms		{ "x32", "x64" }
 
 	project "libpsinc"
 		kind				"SharedLib"
 		targetname			"psinc"
 		links				{ "emergent", "usb-1.0" }
 		files				{ "include/psinc/**h", "src/psinc/**.cpp" }
+		
 		configuration "linux"
 			postbuildcommands	"./strip lib/libpsinc.so"
+			
 		configuration "not vs*"
 			flags			"Symbols"
 			buildoptions	{ "-Wall", "-Wno-sign-compare", "-std=c++11", "-O3", "-D_FORTIFY_SOURCE=2" }
 			linkoptions 	{ "-Wl,-soname,libpsinc.so.0" }
+			
 		configuration "vs*"
+			configurations { "debug", "release" }
 			kind "StaticLib"
 			configuration "debug"
-				flags "Symbols"
-				targetname "psincd"
+				flags		"Symbols"
+				targetname	"psincd"
 			configuration "release"
-				flags "Optimize"
+				optimize	"Full"
 
