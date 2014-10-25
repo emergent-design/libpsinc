f4ef3e3aec07e632983c6f8c44483358266fd0ed
Dan Parnham <dan@emergent-design.co.uk>
2014-10-26 00:41:08 +0100

    Updated to build with clang under linux.

--- cpp/premake5.lua
+++ cpp/premake5.lua
@@ -4,7 +4,9 @@ solution "psinc"
 	includedirs		"include"
 	libdirs			{ "lib" }
 	excludes		{ "**.bak", "**~" }
-	
+
+	configuration "linux"
+		toolset "clang"
 	configuration "not vs*"
 		configurations  { "default" }
 		platforms		"native"
@@ -17,20 +19,20 @@ solution "psinc"
 		targetname			"psinc"
 		links				{ "emergent", "usb-1.0" }
 		files				{ "include/psinc/**h", "src/psinc/**.cpp" }
-		
+
 		configuration "linux"
 			postbuildcommands	"./strip lib/libpsinc.so"
-			
+
 		configuration "not vs*"
 			flags			"Symbols"
 			buildoptions	{ "-Wall", "-Wno-sign-compare", "-std=c++11", "-O3", "-D_FORTIFY_SOURCE=2" }
 			linkoptions 	{ "-Wl,-soname,libpsinc.so.0" }
-			
+
 		configuration "vs*"
 			kind		"StaticLib"
 			defines		"NOMINMAX"
 			targetname	"psinc_%{cfg.buildcfg}_%{cfg.platform}"
-			
+
 			configuration "debug"
 				flags		"Symbols"
 			configuration "release"
